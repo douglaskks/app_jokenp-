d7e054bdfdccfbc1520c9b98b7268ff45850c53c
douglaskks <douglaszxv2@gmail.com>
2023-12-26 16:03:05 -0300

    Interface do app criada

---
+++ lib/jogo.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class Jogo extends StatefulWidget {
+  const Jogo({super.key});
+
+  @override
+  State<Jogo> createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Joken Po",
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text(
+                "Escolha do APP",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+          Image.asset('images/padrao.png'),
+          Padding(
+              padding: EdgeInsets.only(top: 32, bottom: 16),
+              child: Text(
+                "Escolha a sua opção:",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              )),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Image.asset(
+                'images/pedra.png',
+                height: 95,
+              ),
+              Image.asset(
+                'images/papel.png',
+                height: 95,
+              ),
+              Image.asset(
+                'images/tesoura.png',
+                height: 95,
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
